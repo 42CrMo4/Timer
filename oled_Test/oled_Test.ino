77a0dcd0f658da8cdf986543967dfb0c2638c29a
42CrMo4 <44754810+42CrMo4@users.noreply.github.com>
2021-10-13 19:55:43 +0200

    Update oled_Test.ino

--- oled_Test/oled_Test.ino
+++ oled_Test/oled_Test.ino
@@ -1,4 +1,4 @@
-// Test u8g2 OLED
+// Test U8X8 OLED
 // MegaTinyCore 2.3.2
 //   Chip: ATtiny1624
 //   Clock: 10MHz
@@ -6,24 +6,23 @@
 //   Voltage for UART: Closer to 3V
 //   Startup Time: 8mS
 
-#include <Arduino.h>
-#include <Wire.h>
-#include <U8g2lib.h> // https://github.com/olikraus/u8g2 v2.28.10
+#include <U8x8lib.h> // https://github.com/olikraus/u8g2  v2.28.10
 
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0,/* reset=*/ U8X8_PIN_NONE); 
+U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
-//int pinLED1 = PIN_PC0;
+//int pinLED1 = PIN_PA6;
 int counter = 0;
 
 void setup() {
-//  delay(500);
+  delay(500);
 //  pinMode(pinLED1, OUTPUT);
 //  digitalWrite(pinLED1,LOW);
 //  Serial.begin(115200);
 //  Serial.print("Test");
-  u8g2.begin();
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_helvR18_tr);
+  delay(100);
+  u8x8.begin();
+  u8x8.setFlipMode(0);
+  u8x8.setFont(u8x8_font_courB24_3x4_n);
 //  digitalWrite(pinLED1,HIGH);    
 }
 
@@ -31,8 +30,8 @@ void loop() {
   delay(500);
 //  digitalWrite(pinLED1, !(digitalRead(pinLED1)));
 //  Serial.print(counter);
-  u8g2.setCursor(0, 30);
-  u8g2.print(counter);
-  u8g2.sendBuffer();  
+  u8x8.setCursor(0, 0);
+  u8x8.print(counter);
+  u8x8.print(":");
   counter++;
 }
