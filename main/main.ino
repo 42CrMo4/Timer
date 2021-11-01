a7050fbceb1d325edc19e34024996b767f70c813
42CrMo4 <44754810+42CrMo4@users.noreply.github.com>
2021-11-01 14:57:15 +0100

    add all buttons

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,3 @@
-// Test U8X8 OLED
-// MegaTinyCore 2.3.2
-//   Chip: ATtiny1624
-//   Clock: 10MHz
-//   millis()/micros() TCB1
-//   Voltage for UART: Closer to 3V
-//   Startup Time: 8mS
-
 #include <U8x8lib.h> // https://github.com/olikraus/u8g2
 #include <ezButton.h>
 
@@ -15,7 +7,16 @@ U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 //int but2 = PIN_PC3;
 
 ezButton button1(PIN_PC0);
-ezButton button2(PIN_PC3);
+ezButton button2(PIN_PA5);
+ezButton button3(PIN_PB4);
+ezButton button4(PIN_PC1);
+ezButton button5(PIN_PA3);
+ezButton button6(PIN_PB5);
+ezButton button7(PIN_PC2);
+ezButton button8(PIN_PA2);
+ezButton button9(PIN_PA7);
+ezButton button0(PIN_PA1);
+ezButton buttonStart(PIN_PC3);
 ezButton buttonReset(PIN_PA6);
 
 // hh:mm:ss
@@ -43,9 +44,20 @@ bool countown_done = false;
 bool reset_yes = false;
 
 void setup() {
-  pinConfigure(PIN_PC0, (PIN_DIR_INPUT | PIN_PULLUP_ON));
-  pinConfigure(PIN_PC3, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PA1, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PA2, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PA3, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PA5, (PIN_DIR_INPUT | PIN_PULLUP_ON));
   pinConfigure(PIN_PA6, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PA7, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PB4, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PB5, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PC0, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PC1, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PC2, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+  pinConfigure(PIN_PC3, (PIN_DIR_INPUT | PIN_PULLUP_ON));
+
+  pinConfigure(PIN_PA4, (PIN_DIR_OUTPUT));
   delay(100);
   u8x8.begin();
   u8x8.setFlipMode(0);
@@ -53,6 +65,15 @@ void setup() {
 
   button1.setDebounceTime(50); // set debounce time to 50 milliseconds
   button2.setDebounceTime(50);
+  button3.setDebounceTime(50);
+  button4.setDebounceTime(50);
+  button5.setDebounceTime(50);
+  button6.setDebounceTime(50);
+  button7.setDebounceTime(50);
+  button8.setDebounceTime(50);
+  button9.setDebounceTime(50);
+  button0.setDebounceTime(50);
+  buttonStart.setDebounceTime(50);
   buttonReset.setDebounceTime(50);
 
   startMillis = millis();  //initial start time
@@ -61,13 +82,56 @@ void setup() {
 void loop() {
   button1.loop(); // MUST call the loop() function first
   button2.loop();
+  button3.loop();
+  button4.loop();
+  button5.loop();
+  button6.loop();
+  button7.loop();
+  button8.loop();
+  button9.loop();
+  button0.loop();
+  buttonStart.loop();
   buttonReset.loop();
 
   if (button1.isReleased()) {
-    x = 2;
+    x = 1;
     number();
   }
   if (button2.isReleased()) {
+    x = 2;
+    number();
+  }
+  if (button3.isReleased()) {
+    x = 3;
+    number();
+  }
+  if (button4.isReleased()) {
+    x = 4;
+    number();
+  }
+  if (button5.isReleased()) {
+    x = 5;
+    number();
+  }
+  if (button6.isReleased()) {
+    x = 6;
+    number();
+  }
+  if (button7.isReleased()) {
+    x = 7;
+    number();
+  }
+  if (button8.isReleased()) {
+    x = 8;
+    number();
+  }if (button9.isReleased()) {
+    x = 9;
+    number();
+  }if (button0.isReleased()) {
+    x = 0;
+    number();
+  }
+  if (buttonStart.isReleased()) {
     reset_yes = false;
     if (hms > 0) {
       start_timer();
@@ -120,7 +184,7 @@ void counter(int k) {
   currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
   if (currentMillis - startMillis >= period)  //test whether the period has elapsed
   {
-    startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
+    startMillis = currentMillis;  //IMPORTANT to save the start time
     hms = (h2 * 10 + h1) * 3600 + (m2 * 10 + m1) * 60 + s2 * 10 + s1;
     hms = hms + k;
     h = hms / 3600;
@@ -149,6 +213,9 @@ void start_timer() {
     if (hms == 0){
       countown_done = true;
       u8x8.clearDisplay();
+      digitalWrite(PIN_PA4, HIGH);
+      delay(500);
+      digitalWrite(PIN_PA4, LOW);
       countup();
     }
   }
